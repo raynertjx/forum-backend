c1d8c1c430e6aeaa78ef02c141eb08113412a1ef
raynertjx <rayner742@gmail.com>
2022-12-14 23:14:16 +0800

    added user controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,36 @@
 class UsersController < ApplicationController
+    before_action :authorized, only: [:auto_login]
+
+    # registering user
     def create
+        @user = User.create(user_params)
+        if @user.valid?
+            token = encode_token({user_id: @user.id})
+            render json: {user: @user, token: token}
+        else
+            render json: {error: "Invalid username or password"}
+        end
     end
 
+    # logging in user
+    def login
+        @user = User.find_by(username: params[:username])
+
+        if @user && @user.authenticate(params[:password])
+            token = encode_token({user_id: @user.id})
+            render json: {user: @user, token: token}
+        else
+            render json: {error: "Invalid username or password"}
+        end
+    end
+
+    def auto_login
+        render json: @user
+    end
+    
+    private
+
     def user_params
+        params.permit(:username, :password)
+    end
 end
