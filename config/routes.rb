44cc1ce07d42f1382d04002989073ffd23d22c54
raynertjx <rayner742@gmail.com>
2022-12-14 20:53:15 +0800

    modified routes for login authentication

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  Rails.application.routes.draw do
+    resources :users, only: [:create]
+    post '/login', to: "users#login"
+    get '/auto_login', to: "users#auto_login"
+  end
 end
