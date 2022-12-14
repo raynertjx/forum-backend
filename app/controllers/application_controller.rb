6aaa7382023fedf9222c858b5b6f7fcb0fa71e87
raynertjx <rayner742@gmail.com>
2022-12-14 20:52:24 +0800

    added jwt token authentication methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,38 @@
 class ApplicationController < ActionController::API
+    before_action :authorized
+
+    # secret should be ENV variable in production!
+    def encode_token(payload)
+        JWT.encode(payload, 'secret')
+    end
+
+    def auth_header
+        request.headers['Authorization']
+    end
+
+    def decode_token
+        if auth_header
+            token = auth_header.split(' ')[1]
+            begin
+                JWT.decode(token, 'secret', true, algorithm: 'HS256')
+            rescue JWT::DecodeError
+                nil
+            end
+        end
+    end
+
+    def logged_in_user
+        if decode_token
+            user_id = decode_token[0]['user_id']
+            @user = User.find_by(id: user_id)
+        end
+    end
+
+    def logged_in?
+        !!logged_in_user
+    end
+
+    def authorized
+        render json: {message: 'Please log in!'}, status: :unauthorized unless logged_in?
+    end
 end
