7bd919b5167a41ac114e8b568907f047437c5f58
raynertjx <rayner742@gmail.com>
2022-12-19 23:53:59 +0800

    set credentials to true

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
-
-    resource "*",
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    origins 'http://localhost:5173'
+    resource '*', headers: :any, methods: [:get, :post, :patch, :put],
+    credentials: true
   end
 end
