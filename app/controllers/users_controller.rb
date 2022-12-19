7ceca1dabb58d7103d1b75ea793b8ff5b7d9735a
raynertjx <rayner742@gmail.com>
2022-12-20 00:07:15 +0800

    modified login & logout methods to use httponly cookies

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
-    before_action :authorized, only: [:auto_login]
 
-    # registering user
+    # signing up user
     def create
         @user = User.create(user_params)
         if @user.valid?
             token = encode_token({user_id: @user.id})
+            cookies.signed[:jwt] = {value: token, httponly: true, expires: 1.hour.from_now}
             render json: {user: @user, token: token}
         else
             render json: {error: "Invalid username or password"}
@@ -15,17 +15,17 @@ class UsersController < ApplicationController
     # logging in user
     def login
         @user = User.find_by(username: params[:username])
-
         if @user && @user.authenticate(params[:password])
             token = encode_token({user_id: @user.id})
+            cookies.signed[:jwt] = {value: token, httponly: true, expires: 1.hour.from_now}
             render json: {user: @user, token: token}
         else
             render json: {error: "Invalid username or password"}
         end
     end
 
-    def auto_login
-        render json: @user
+    def logout
+        cookies.delete(:jwt)
     end
     
     private
