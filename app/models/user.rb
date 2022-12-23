2312df082b88537facf9457c77909f15e1785076
raynertjx <rayner742@gmail.com>
2022-12-23 15:53:42 +0800

    modified user model for improved user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
-
-    validates_presence_of :username 
-    validates_uniqueness_of :username
+    validates :username, presence: true, uniqueness: true
+    validates :password,
+              length: { minimum: 6 },
+              if: -> { new_record? || !password.nil? }
 end
