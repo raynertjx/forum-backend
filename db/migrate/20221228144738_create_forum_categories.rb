939fac37935d2f9c7da0c7fb030ffd8d605f2c17
raynertjx <rayner742@gmail.com>
2022-12-28 22:48:28 +0800

    initialised ForumCategories model

---
+++ db/migrate/20221228144738_create_forum_categories.rb
@@ -0,0 +1,12 @@
+class CreateForumCategories < ActiveRecord::Migration[7.0]
+  def change
+    create_table :forum_categories do |t|
+      t.string :name
+      t.string :latest_thread
+      t.string :comments_count
+      t.string :number
+
+      t.timestamps
+    end
+  end
+end
