a6b592f130c3f25219aa9753a966d43e7fe175e0
raynertjx <rayner742@gmail.com>
2023-01-12 14:07:29 +0800

    added route to get comments for specific forum thread

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,13 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   Rails.application.routes.draw do
-  resources :forum_comments
+    resources :forum_comments
     resources :forum_threads
     resources :users, only: [:create]
     get '/forum_categories', to: "forum_categories#index"
     patch '/forum_categories/:forum', to: "forum_categories#update"
     get '/index_category/:forum_category_id', to: "forum_threads#index_category"
+    get '/index_thread/:forum_thread_id', to: "forum_comments#index_thread"
     post '/login', to: "users#login"
     post '/signup', to: "users#create"
     post '/logout', to: "users#logout"
