43c593720fd4de9c68baa96c110290b02d2be38f
raynertjx <rayner742@gmail.com>
2023-01-12 14:07:55 +0800

    added index_thread method

--- app/controllers/forum_comments_controller.rb
+++ app/controllers/forum_comments_controller.rb
@@ -1,11 +1,18 @@
 class ForumCommentsController < ApplicationController
   before_action :set_forum_comment, only: %i[ show update destroy ]
+  before_action :authorized
+  before_action :owner?, only: %i[edit destroy]
 
   # GET /forum_comments
   def index
     @forum_comments = ForumComment.all
-
     render json: @forum_comments
+  end
+
+  # GET comments from a specific forum thread
+  def index_thread
+    @comments = ForumComment.all.select { |comment| comment.forum_thread_id == Integer(params[:forum_thread_id])}
+    render json: @comments
   end
 
   # GET /forum_comments/1
@@ -16,7 +23,8 @@ class ForumCommentsController < ApplicationController
   # POST /forum_comments
   def create
     @forum_comment = ForumComment.new(forum_comment_params)
-
+    @forum_comment.user_id = @user.id
+    @forum_comment.author = @user.username
     if @forum_comment.save
       render json: @forum_comment, status: :created, location: @forum_comment
     else
@@ -44,8 +52,19 @@ class ForumCommentsController < ApplicationController
       @forum_comment = ForumComment.find(params[:id])
     end
 
+    # Get comments for a particular thread.
+    # def set_thread_comments
+    #   @thread_comments = ForumComment.all.select { |comment| comment.forum_thread_id == Integer(params[:forum_thread_id])}
+    # end
+
+    def owner?
+      unless @user.id == @forum_comment.user_id
+        redirect_back fallback_location: root_path, notice: 'User is not owner'
+      end
+    end
+
     # Only allow a list of trusted parameters through.
     def forum_comment_params
-      params.require(:forum_comment).permit(:content, :author, :user_id, :ForumThread_id)
+      params.require(:forum_comment).permit(:content, :forum_thread_id).except(:author, :user_id)
     end
 end
