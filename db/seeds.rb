d2f8fc65defb75e859f2579da9332671b3f7b782
raynertjx <rayner742@gmail.com>
2023-01-24 23:06:04 +0800

    added db seed:

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+ForumCategory.create(name: "University Life", latest_thread: "test", thread_count: 0, url:"university-life", subtitle: "General discussion about life in NUS!")
+ForumCategory.create(name: "Module Reviews", latest_thread: "test", thread_count: 0, url:"module-reviews", subtitle: "Discuss academic modules!")
+ForumCategory.create(name: "Interest Groups", latest_thread: "test", thread_count: 0, url:"interest-groups", subtitle: "Explore your interests with others!")
+ForumCategory.create(name: "Food Reviews", latest_thread: "test", thread_count: 0, url:"food-reviews", subtitle: "Find out what to have for lunch!")
+ForumCategory.create(name: "Homework Help", latest_thread: "test", thread_count: 0, url:"homework-help", subtitle: "Your SOS when your assignment is due!")
+
