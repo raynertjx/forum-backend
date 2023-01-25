b7a5b93d7e0e6322a4dabc4d9732e8b92ba4cc28
raynertjx <rayner742@gmail.com>
2023-01-25 14:36:24 +0800

    fixed wildcard resource

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'http://localhost:5173', 'https://symphonious-dango-f8e9aa.netlify.app/'
     resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete],
     credentials: true
   end
